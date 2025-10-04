00c035528889f9f38bb2c9761a264e06aa2b76b3
Khizar Ahmed <mikhizarahmed@gmail.com>
2025-10-04 14:19:29 +0000

    my first program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,1 +1,24 @@
 // Test file
+#include <stdio.h>
+int main() 
+{
+    long a;
+
+    char b[] = "hello"; 
+
+   // long -32000 to +32000
+
+    bool yesorno = true;
+
+    printf("%ld \n",a);
+
+    printf("%s ",b);
+
+    return 0;
+}
+
+class Student {
+    int rollnumber;
+    char studentname[25];
+    char classname[5];
+}
